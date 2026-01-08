d22090083faef0a94c852c24eb103776d01b09c0
Jacobo-Dominguez <jacobo9810@gmail.com>
2026-01-08 19:11:27 +0100

    Ruta de las vistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JuegosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta para el detalle de un juego
+Route::get('/juegos/{id}', [JuegosController::class, 'show'])->name('juegos.show');
