c26d98d8dee18e259e692fb659863c5824c65308
Jacobo-Dominguez <jacobo9810@gmail.com>
2026-01-08 19:10:40 +0100

    Controlador de detalle del Juego

---
+++ app/Http/Controllers/JuegosController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class JuegosController extends Controller
+{
+    public function show($id)
+    {
+        // Cargar el array de juegos desde el archivo Data
+        $juegos = require app_path('Data/juegos.php');
+        
+        // Buscar el juego por ID
+        $juego = collect($juegos)->firstWhere('id', (int)$id);
+        
+        // Si no se encuentra el juego, mostrar error 404
+        if (!$juego) {
+            abort(404, 'Juego no encontrado');
+        }
+        
+        // Devuelve la vista con el juego encontrado
+        return view('juegos.show', compact('juego'));
+    }
+}
