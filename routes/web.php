55f3decaf51e6f5efe3a7eb38bb38980a61f6735
David Gutiérrez <empirio14@alumnos.ilerna.com>
2026-01-09 19:45:35 +0100

    Subo últimos cambios

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,5 @@ use App\Http\Controllers\JuegosDetalleController;
 
 Route::get('/juegos', [JuegosDetalleController::class, 'index'])->name('juegos.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Ruta para el detalle de un juego
 Route::get('/juegos/{id}', [JuegosDetalleController::class, 'show'])->name('juegos.show');
